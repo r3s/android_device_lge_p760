2373148d957f36f847ad0cc5e1879286835a4bf6
Rammar515 <rammarad515@gmail.com>
2014-12-15 21:35:51 +0100

    Update NFC for CM12

--- device.mk
+++ device.mk
@@ -26,21 +26,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
     lge.nfc.indicator=yes \
     lge.nfc.defaultonoff=on
 
-# NFC stuffs
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/nfcee_access.xml:system/etc/nfcee_access.xml
-
 # Permission files
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
-    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
-    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml
+    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml
 
 # NFC packages
 PRODUCT_PACKAGES += \
     libnfc \
     libnfc_jni \
     Nfc \
-    Tag \
-    com.android.nfc_extras
+    Tag
 
